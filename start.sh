653b4715feab3954723f4c2213d07e0537d3a2e6
LitoNeo <auto.hongda@gmail.com>
2019-04-04 15:13:31 +0800

    add start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 # remove default gateway for wlan and velodyne-wired-connection
 sudo route del default gw 192.168.1.1
-
+sudo modprobe can
+sudo modprobe can_raw
+sudo modprobe mttcan
+sudo ip link set can0 type can bitrate 500000 dbitrate 2000000 berr-reporting on fd on
+sudo ip link set up can0
